85e61491be01d12353ece5f3f0414e759fbbc4ed
Ronuk Raval <ronuk.raval@gmail.com>
2015-01-31 20:45:23 -0500

    fix typo in setup.py

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup
 
 setup(
     name='gplay-devices',
-    version='0.1',
-    py_modules=['gplay-devices'],
+    version='0.1.1',
+    py_modules=['gplay_devices'],
 
     author='Ronuk Raval',
     author_email='ronuk.raval@gmail.com',
